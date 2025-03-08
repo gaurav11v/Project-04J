3b4bcd83c7b503a2aa569e62a0f8c8e11a9bbb53
Gaurav Verma <gv.verma111994@gmail.com>
2025-03-08 13:05:06 +0530

    done

---
+++ ORS_Project4/src/main/java/com/rays/pro4/Exception/DatabaseException.java
@@ -0,0 +1,19 @@
+package com.rays.pro4.Exception;
+
+/**
+ * DatabaseException is propogated by Model classes when an unhandled Database
+ * exception occurred.
+ * 
+ * @author Gaurav Verma
+ *
+ */
+
+public class DatabaseException extends Exception {
+
+	public DatabaseException(String msg) {
+		super(msg);
+	}
+
+}
+ 
+ 
---
+++ ORS_Project4/src/main/java/com/rays/pro4/Exception/RecordNotFoundException.java
@@ -0,0 +1,13 @@
+package com.rays.pro4.Exception;
+
+/**
+ * RecordNotFoundException thrown when a record not found occurred.
+ * 
+ * @author Gaurav Verma
+ * 
+ */
+public class RecordNotFoundException extends Exception {
+	public RecordNotFoundException(String msg) {
+		super(msg);
+	}
+}
